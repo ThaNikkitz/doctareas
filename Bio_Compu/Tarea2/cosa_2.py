3263c696f26d14dcb92a526fa28a91d5fe8205a2
ThaNikkitz <nico.espinozam@gmail.com>
2020-10-29 22:30:31 -0300

    Empece el informe, esperando resultados. Falta dibujar monitos y hacer el par de plots (energia y tiempo)

--- Bio_Compu/Tarea2/cosa_2.py
+++ Bio_Compu/Tarea2/cosa_2.py
@@ -1,16 +1,16 @@
 from simtk import openmm, unit
 from openmmtools import testsystems
 import time
+import sys
 
+L_side = 4.0 #nm
+pme_cutoff = 1.5 #nm. <L_side/2 
 
-L_side = 5.0 #nm
-pme_cutoff = 2.5 #nm. <L_side/2 
-
-N_steps = 100
+N_steps = 200
 
 # Try: nonbondedMethod: NoCutoff, Ewald, PME
 
-water_box = testsystems.WaterBox(box_edge=L_side*unit.nanometer, nonbondedMethod = openmm.app.NoCutoff, nonbondedCutoff=pme_cutoff*unit.nanometers)
+water_box = testsystems.WaterBox(box_edge=L_side*unit.nanometer, nonbondedMethod = openmm.app.PME, nonbondedCutoff=pme_cutoff*unit.nanometers, ewaldErrorTolerance = 0.001)
 system = water_box.system
 
 print("Water box with %i atoms"%len(water_box.positions))
@@ -18,6 +18,7 @@ print("Water box with %i atoms"%len(water_box.positions))
 integrator = openmm.LangevinIntegrator(300*unit.kelvin, 1/unit.picosecond, 0.002*unit.picoseconds)
 simulation = openmm.app.simulation.Simulation(water_box.topology, system, integrator)
 simulation.context.setPositions(water_box.positions)
+simulation.reporters.append(openmm.app.StateDataReporter(sys.stdout, N_steps, step = False, potentialEnergy = True))
 
 tic = time.time()
 simulation.step(N_steps)
--- Bio_Compu/Tarea2/simple_box.py
+++ Bio_Compu/Tarea2/simple_box.py
@@ -2,11 +2,12 @@ from simtk import openmm, unit
 from openmmtools import testsystems
 import numpy
 import time
+from sys import stdout
 
-file1 = open('./N-var2.dat', 'w+')
+file1 = open('./N-var9.dat', 'w+')
 
 L_side = 6.0 #nm
-pme_cutoff = 2.0 #nm, < L_side/2
+pme_cutoff = 2.5 #nm, < L_side/2
 file1.write('%1.3f \t %1.3f\n\n'%(L_side, pme_cutoff))
 
 for Method in [openmm.app.NoCutoff ,openmm.app.Ewald, openmm.app.PME]:
@@ -14,23 +15,28 @@ for Method in [openmm.app.NoCutoff ,openmm.app.Ewald, openmm.app.PME]:
     print(Method)
     file1.write('\t' + str(Method) + '\n\n')
 
-    Ns = numpy.arange(150, 310, 10)
+    Ns = numpy.arange(200, 1200, 200)
     file1.write('N\t\t time\n')
     for N in Ns:
-        print(N)
+#        print(N)
         N_steps = int(N)
 
 # Try: nonbondedMethod: NoCutoff, Ewald, PME
+        if str(Method) == "NoCutoff":
+             water_box = testsystems.WaterBox(box_edge=L_side*unit.nanometer, nonbondedMethod = Method)
+#             print(str(Method) + 'check')
 
-        water_box = testsystems.WaterBox(box_edge=L_side*unit.nanometer, nonbondedMethod = Method, nonbondedCutoff=pme_cutoff*unit.nanometers)
+        else:
+            water_box = testsystems.WaterBox(box_edge=L_side*unit.nanometer, nonbondedMethod = Method, nonbondedCutoff=pme_cutoff*unit.nanometers, ewaldErrorTolerance = 0.001)
         system = water_box.system
 
         integrator = openmm.LangevinIntegrator(300*unit.kelvin, 1/unit.picosecond, 2*unit.femtoseconds)
         simulation = openmm.app.simulation.Simulation(water_box.topology, system, integrator)
         simulation.context.setPositions(water_box.positions)
+        simulation.reporters.append(openmm.app.StateDataReporter(stdout, N_steps, step = False, potentialEnergy = True))
 
-        if N == 200:
-            print("check!")
+#        if N == 500:
+#            print("check!")
 
         tic = time.time()
         simulation.step(N_steps)
